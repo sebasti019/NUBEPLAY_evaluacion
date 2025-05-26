0a599e8eb8878f3497dbb21205da8f3e106f4513
BMORA <ben.morag@duocuc.cl>
2025-05-26 01:12:55 -0400

    Benjamora

---
+++ src/main/java/com/example/NUBEPLAY_INVENTARIO/NubeplayInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.NUBEPLAY_INVENTARIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubeplayInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NubeplayInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NUBEPLAY_INVENTARIO/NubeplayInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NUBEPLAY_INVENTARIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubeplayInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
