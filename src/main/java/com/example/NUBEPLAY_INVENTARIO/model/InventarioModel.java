ea05fe9e6174716f69bcc7317b9d0935a02862d9
BMORA <ben.morag@duocuc.cl>
2025-05-26 03:40:42 -0400

    Conexion Base de datos Inventario

--- src/main/java/com/example/NUBEPLAY_INVENTARIO/model/InventarioModel.java
+++ src/main/java/com/example/NUBEPLAY_INVENTARIO/model/InventarioModel.java
@@ -1,22 +1,30 @@
 package com.example.NUBEPLAY_INVENTARIO.model;
+
 import jakarta.persistence.*;
 import lombok.Data;
+
 @Entity
-@Table(name = &quot;inventario&quot;)
+@Table(name = "inventario")
 @Data
 public class InventarioModel {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = &quot;id_juego&quot;)
-    private int idJuego;
-    @Column(name = &quot;titulo_juego&quot;, nullable = false)
-    private String tituloJuego;
-    @Column(name = &quot;categoria&quot;, nullable = false)
-    private String categoria;
-    @Column(name = &quot;stock&quot;)
-    private int stock;
-    @Column(name = &quot;precio&quot;)
-    private double precio;
-    @Column(name = &quot;rating_contenido&quot;)
-    private int ratingContenido;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_juego")
+    private int idJuego;
+
+    @Column(name = "titulo_juego", nullable = false)
+    private String tituloJuego;
+
+    @Column(name = "categoria", nullable = false)
+    private String categoria;
+
+    @Column(name = "stock")
+    private int stock;
+
+    @Column(name = "precio")
+    private double precio;
+
+    @Column(name = "rating_contenido")
+    private int ratingContenido;
 }
--- src/main/java/com/example/NUBEPLAY_INVENTARIO/repository/inventarioRepository.java
+++ src/main/java/com/example/NUBEPLAY_INVENTARIO/repository/inventarioRepository.java
@@ -1,11 +1,13 @@
 package com.example.NUBEPLAY_INVENTARIO.repository;
+
 import com.example.NUBEPLAY_INVENTARIO.model.InventarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
+
 @Repository
-public interface InventarioRepository extends JpaRepository&lt;InventarioModel,
-Integer&gt; {
-    List&lt;InventarioModel&gt; findByCategoriaIgnoreCase(String categoria);
-    List&lt;InventarioModel&gt; findByStockLessThan(int stock);
+public interface inventarioRepository extends JpaRepository<InventarioModel, Integer> {
+    List<InventarioModel> findByCategoriaIgnoreCase(String categoria);
+    List<InventarioModel> findByStockLessThan(int stock);
 }
